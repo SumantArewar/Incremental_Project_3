0d1d9570e16c66344c9f6f47231cd44bd7f5cdad
Iamneo <techadmin@iamneo.ai>
2023-11-17 19:37:56 +0000

    Backup Commit

--- dotnetapp/Models/Player.cs
+++ dotnetapp/Models/Player.cs
@@ -1,4 +1,20 @@
-﻿namespace dotnetapp.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace dotnetapp.Models
 {
-    
+    public class Player
+    {
+        [Required]
+        public int Id{get;set;}
+        [Required(ErrorMessage = "Name is required.")]
+        public string ?Name{get;set;}
+        [Required]
+        public string ?Category{get;set;}
+        
+        [Range(0.01, int.MaxValue , ErrorMessage ="Bidding amount must be greater than 0.")]
+        public decimal BiddingAmount{get;set;} 
+    }
 }
