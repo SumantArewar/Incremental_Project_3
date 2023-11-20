c051dfaf2ff3826704f4216d925e8de1a918ec2c
Iamneo <techadmin@iamneo.ai>
2023-11-20 20:06:54 +0000

    Backup Commit

--- angularapp/src/app/error/error.component.ts
+++ angularapp/src/app/error/error.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Player } from 'src/models/player';
+import { AdminService } from '../services/admin.service';
 
 @Component({
   selector: 'app-error',
@@ -7,12 +9,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./error.component.css']
 })
 export class ErrorComponent implements OnInit {
+  playerdata : Player
+  id : number 
 
-  constructor(private router : Router){
-    this.router.navigate(['/admin']);
+  constructor(private ms : AdminService , private ar : ActivatedRoute , private route : Router) { }
+
+  ngOnInit() 
+  {
+    const tid = this.ar.snapshot.paramMap.get('id')
+    this.id = Number(tid)
+    this.ms.getPlayer(this.id).subscribe((data : Player) =>{
+      this.playerdata = data
+    })
+  }
+  deleteData(id :number)
+  {
+    this.id = id
+    this.ms.deletePlayer(this.id).subscribe(()=>{
+      alert("Record Deleted")
+      this.route.navigate(['/listmovies'])
+    })
   }
 
-  ngOnInit(): void {
-  }
+  
 
 }
