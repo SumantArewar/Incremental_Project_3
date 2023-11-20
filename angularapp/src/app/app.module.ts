d9172f3d3da198ba7c424a547adad584975cea3a
Iamneo <techadmin@iamneo.ai>
2023-11-20 04:19:22 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     LoginComponent,
     PlayerComponent,
-    RegistrationComponent,``
+    RegistrationComponent,
     AdminComponent,
     OrganizerComponent,
     HomeComponent,
