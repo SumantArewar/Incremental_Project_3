e3639cf4189b6b80b3ae49b6398258fc135efea0
Iamneo <techadmin@iamneo.ai>
2023-11-23 15:22:15 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminService } from '../services/admin.service';
-import { RegistrationComponent } from '../registration/registration.component';
-import { LoginComponent } from '../login/login.component';
-import { AdminComponent } from '../admin/admin.component';
-import { OrganizerComponent } from '../organizer/organizer.component';
-import { HomeComponent } from '../home/home.component';
-import { ErrorComponent } from '../error/error.component';
+import { AdminService } from './services/admin.service';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   // {path : '' , redirectTo :'/home' , pathMatch :'full'},
--- angularapp/src/app/player/player.component.ts
+++ angularapp/src/app/player/player.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from 'src/models/player';
+import { AdminService } from '../services/admin.service';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-player',
@@ -12,8 +15,12 @@ export class PlayerComponent implements OnInit {
   // player = { id: 1, name: 'Test Player', age: 25, category: 'Category A', biddingPrice: 100 };  
   newPlayer  = {biddingPrice : 200};
 
-  constructor() { }
-
+  // players : any[] = []
+  constructor(private ad : AdminService  , private at : AuthService , private router : Router) 
+  {
+    this.ad.getPlayers().subscribe(data => {this.players.push(...data)})
+    console.log(this.players)
+  }
   ngOnInit(): void {
   }
 
