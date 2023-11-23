d4cfd21ab28a73a9685a77eebf70e2dcb9d47a9a
Iamneo <techadmin@iamneo.ai>
2023-11-23 14:07:15 +0000

    Backup Commit

--- angularapp/src/app/routing/app-routing.module.ts
+++ angularapp/src/app/routing/app-routing.module.ts
@@ -9,15 +9,30 @@ import { HomeComponent } from '../home/home.component';
 import { ErrorComponent } from '../error/error.component';
 
 const routes: Routes = [
-  {path : '' , redirectTo :'/home' , pathMatch :'full'},
-  // { path: 'login', component: LoginComponent },
-  // { path: '', component: HomeComponent },
-  { path: 'admin', component: AdminComponent },
-  { path: 'organizer', component: OrganizerComponent },
-  { path: 'error', component: ErrorComponent },
-  { path: 'error', component: ErrorComponent, data: { message: 'Oops! Something went wrong.' } },
-  { path: 'register', component: RegistrationComponent },
-  { path: '**', redirectTo: '/error', pathMatch: 'full' }
+  // {path : '' , redirectTo :'/home' , pathMatch :'full'},
+  // // { path: 'login', component: LoginComponent },
+  // // { path: '', component: HomeComponent },
+  // { path: 'admin', component: AdminComponent },
+  // { path: 'organizer', component: OrganizerComponent },
+  // { path: 'error', component: ErrorComponent },
+  // { path: 'error', component: ErrorComponent, data: { message: 'Oops! Something went wrong.' } },
+  // { path: 'register', component: RegistrationComponent },
+  // { path: '**', redirectTo: '/error', pathMatch: 'full' }
+  {path : 'listteams', component : ListTeamsComponent},
+  {path : 'listteam/:id', component : FindTeamComponent},
+  {path : 'addplayer', component : AddPlayerComponent},
+  {path : 'addteam', component : AddTeamComponent},
+  {path : 'editteam/:id', component : EditTeamComponent},
+  {path : 'deleteteam/:id', component : DeleteTeamComponent},
+  {path : 'listplayer', component : ListPlayerComponent},
+  {path : 'editplayer/:id', component : EditPlayerComponent},
+  {path : 'deleteplayer/:id', component : DeletePlayerComponent},
+  {path:'admin',component:AdminComponent},
+  {path:'error',component:ErrorComponent},
+  {path:'organizer',component:OrganizerComponent},
+  {path:'login',component:LoginComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'players',component:PlayerComponent}
 ];
 
 @NgModule({
